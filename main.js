007f24b3b21978e429cd32ec50d6350bbbbdb57d
joshguion <joshguion@icloud.com>
2015-11-02 18:05:03 -0500

    second commit

--- main.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-          url: "https://www.reddit.com/r/cats"
-          method: 'GET'
-          success:function (data) {
-			data.forEach (function (el) {
-				$('body').append()
-			})
-
-});
